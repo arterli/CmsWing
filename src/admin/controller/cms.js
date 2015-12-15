2493d23ce9d048f956daa996650d23d2a09833ff
Arterli <arterli@qq.com>
2015-12-16 02:13:44 +0800

    优化后台登陆页面

--- src/admin/controller/cms.js
+++ src/admin/controller/cms.js
@@ -21,6 +21,13 @@ export default class extends Base {
      * list action
      * @return {Promise} []
      */
+    //id:编号
+    //title:标题:[EDIT]
+    //type:类型
+    //update_time:最后更新
+    //status:状态
+    //view:浏览
+    //id:操作:[EDIT]|编辑,[DELETE]|删除
     async listAction(){
         //auto render template file index_index.html
         let model = this.get('model')
@@ -29,9 +36,41 @@ export default class extends Base {
         //获取模型信息
         model = await this.model('model').where({name:model}).find();
         !think.isEmpty(model) || this.fail("模型不存在！");
-        console.log(model)
+        //解析列表规则
+        let fields = [];
+        let grids  = trim(model.list_grid).split('\r\n');
+        console.log(grids)
+        let ngrids=[];
+        for (var value of grids){
+            if(trim(value) === ''){
+                continue;
+            }
+            // 字段:标题:链接
+            let val      = value.split(':');
+            // 支持多个字段显示
+            let field   = val[0].split(',');
+            let values    = {'field' : field, 'title': val[1]};
+            if(!think.isEmpty(val[2])){
+                values.href	=	val[2];
+            }
+            if(val[1].indexOf('|') > -1){
+                // 显示格式定义
+                [values.title,values.format]    =   val[1].split('|');
+            }
+            for( var v  of field){
+                let array	=	v.split('|');
+                fields.push(array[0]) ;
+            }
+            ngrids.push(values);
+        }
+
+        console.log(ngrids)
+        // 过滤重复字段信息
+        fields=unique(fields);
+        console.log(fields)
         this.meta_title=model.title + '列表';
         this.active = "admin/model/index";
+        this.assign('list_grids', ngrids);
         return this.display();
     }
 }
