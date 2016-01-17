fde71acb1d0a9e5213bbb32993afd4da7f538847
Arterli <arterli@qq.com>
2016-01-17 16:49:17 +0800

    22

--- src/admin/controller/apiadm.js
+++ src/admin/controller/apiadm.js
@@ -7,6 +7,7 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
+
   indexAction(){
     //auto render template file index_index.html
     return this.display();
