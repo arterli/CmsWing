6cb98eacd18ffbcbc0be2293822785743bdc0308
Arterli <arterli@qq.com>
2016-02-03 14:46:51 +0800

    121212

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -9,7 +9,7 @@ export default class extends Base {
    */
   async indexAction(){
     //auto render template file index_index.html
-    this.meta_title = "首页";//标题
+    this.meta_title = "首页";//标题1
     this.keywords = this.setup.WEB_SITE_KEYWORD ? this.setup.WEB_SITE_KEYWORD : '';//seo关键词
     this.description = this.setup.WEB_SITE_DESCRIPTION ? this.setup.WEB_SITE_DESCRIPTION : "";//seo描述
     return this.display();
