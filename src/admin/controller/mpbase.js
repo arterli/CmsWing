6f4cf84738d102b22c90bc74a2d57681d9bfc5a1
lf <ljt310@163.com>
2016-02-26 15:23:06 +0800

    tj

--- src/admin/controller/mpbase.js
+++ src/admin/controller/mpbase.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Base from './base.js';
+import API from 'wechat-api';
 
 export default class extends Base {
     /**
@@ -69,6 +70,183 @@ export default class extends Base {
     async setstatusAction() {
         await super.setstatusAction(this,'member_public');
     }
+    
+    
+     /**
+     * 群发功能
+     */
+     async massAction(){
+        this.meta_title="群发功能";
+
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        //调用用户分组API
+        let aa = function(api){
+            let deferred = think.defer();
+            api.getGroups((err,result)=>{
+                if(!think.isEmpty(result)){
+                    deferred.resolve(result['groups']);
+                }else{
+                    console.error(err);
+                }
+            });
+            return deferred.promise;
+        }
+            let res =await aa(api);
+        this.assign('groups', res);//用户分组
+        this.assign({"navxs": true,"bg": "bg-dark"});
+        return self.display();
+    }
+    
+    /**
+     * 获取微信公众账号用户信息并保存到本地库
+     */
+    async getusersAction(){
+        this.meta_title="获取粉丝信息";
+        let user_model = this.model('user');
+        //let api = new API('wxe8c1b5ac7db990b6', 'ebcd685e93715b3470444cf6b7e763e6');
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        //let finduser = await this.model('user').countSelect();
+        let self = this;
+        //获取关注者列表
+        let users = function(api) {
+            let deferred = think.defer();
+            api.getFollowers((err,result)=>{
+                if(!think.isEmpty(result)){
+                   deferred.resolve(result);
+                }else{
+                    Console.error('err'+err)
+                } 
+            });
+            return deferred.promise;
+        }
+        let res = await users(api);
+        let useropenid = res['data']['openid'];
+        let count = res['count'];
+       //self.end(useropenid);
+        
+        //批量获取用户基本信息
+        let tmp_openids = [];
+        for(let i=0;i<count;i++){
+            tmp_openids.push(useropenid[i]);
+            if((i+1)%100 == 0 || i == (count-1)){
+                //think.log('dd','aaa');
+                 let userinfo = function(api) {
+                    let deferinfo = think.defer();
+                    api.batchGetUsers(tmp_openids,(err,result)=>{
+                        if(!think.isEmpty(result)){
+                        deferinfo.resolve(result);
+                        }else{
+                            Console.error('err'+err);
+                        } 
+                    });
+                    return deferinfo.promise;
+                }
+                let resusers = await userinfo(api);
+                let resinfo = resusers['user_info_list'];
+               //self.end(resinfo);
+                console.log("开始：")
+               for (var key in resinfo) {
+                       var element = resinfo[key];
+                       await user_model.add(element);         
+               }
+               tmp_openids = [];
+            }
+        }       
+    }
+    
+    /**
+     * 群发信息
+     */
+    async bymasssendAction(media_id,receivers){
+        let self = this;
+        self.end(media_id);
+         let mass = function(api){
+            let deferred = think.defer();
+            api.massSendNews(media_id,receivers,(err,result)=>{
+                if(!think.isEmpty(result)){
+                    deferred.resolve(result);
+                }else{
+                    console.error('err'+err);
+                }
+            });
+            return deferred.promise;
+        }
+        let res = await mass(api);
+        self.end(res);
+        return res;
+    }
+    
+
+    /**
+     * 根据条件筛选进行群发--通过openid,只能是认证服务号使用
+     * 通过分组groupid进行群发，认证后的订阅号和服务号都可以使用
+     */
+    async masssendAction(){
+        let model = this.model('user');
+        let self = this;
+        let group_id = this.post('group_id');
+        //let media_id = this.post('media_id');
+        //let group_type = this.post('group_type');
+        //let sex = this.post('sex');
+        //let province = this.post('province');
+        //let city = this.post('city');
+        let media_id = 'ys1SXTdWnZhTZQB77MODarSaJ36xfoTG15deo5aGM3eRvEiuT034AMaUocc66uq9';
+        let group_type = 0;
+        let sex = 1;
+        let province = '陕西';
+        let city = '西安';
+        //通过条件查询本地库数据
+        let userinfo = await model.where({'sex':sex,'province':province,'city':city}).field('openid').select();
+        //let userinfo ='aaa';
+       //self.end(userinfo);
+        
+        let openids = [];
+        for (var key in userinfo) {
+            if (userinfo.hasOwnProperty(key)) {
+                openids.push(userinfo[key].openid);    
+            }
+        }
+        //self.end(aa);
+        let res = '';
+        
+        //判断是通过groupid还是openid进行群发
+        if(group_type == 1){
+            self.end('1');
+            //分组群发
+            res = this.bymasssendAction(media_id,group_id);
+        }else{
+            //self.end('0');
+            if(think.isEmpty(province)){
+                //self.end(province);
+                //全部群发
+                res = this.bymasssendAction(media_id,true);
+            }else{
+                //self.end('openid');
+                //根据条件通过openid进行群发
+                res = this.bymasssendAction(media_id,openids);
+            }
+        }
+        
+        let msg_id = res['msg_id'];//发送成功返回消息ID
+        self.end(msg_id);
+        
+        //判断是否群发消息是否成功
+        if(res['errcode'] == 0){
+            //本地保存media_id和msg_id
+
+        }
+
+        this.assign({"navxs": true,"bg": "bg-dark"});
+        return this.display();
+        
+        
+        
+    }
+
+    
+    
+    
     /**
      * 自定义菜单
      */
--- src/home/controller/wechat.js
+++ src/home/controller/wechat.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Base from './base.js';
+import API from 'wechat-api';
 export default class extends Base {
     /**
      * 微信服务器验证
@@ -30,4 +31,291 @@ export default class extends Base {
   __call(){
     this.reply(DEFULT_AUTO_REPLY);
   }
+  
+   /**
+     * 获取用户分组
+     */
+    groupsAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getGroups((err,result)=>{
+            if(!think.isEmpty(result)){
+                //think.log(result['groups'],"test");
+                for(let val of result['groups']){
+                    think.log(val['name'],"test");
+                };
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+    /**
+     * 查询用户在哪个分组
+     */
+    getwithgroupAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getWhichGroup('oVe9Ew0zHFp0up1CeNcK2J5RL4xs',(err,result)=>{
+            if(!think.isEmpty(result)){
+               think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 创建用户分组
+     */
+    creategroupAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.createGroup('旅游',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 群发文本消息
+     */
+    masssendtextAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.massSendText('你好，恭喜你中奖了',['oVe9Ew0zHFp0up1CeNcK2J5RL4xs','oVe9Ew1nEItmuu-H5NoeZpK0xLzo'],(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+    /**
+     * 群发图文消息
+     */
+    masssendnewsAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.massSendNews('ys1SXTdWnZhTZQB77MODarSaJ36xfoTG15deo5aGM3eRvEiuT034AMaUocc66uq9',['oVe9Ew0zHFp0up1CeNcK2J5RL4xs','oVe9Ew1nEItmuu-H5NoeZpK0xLzo'],(err,result)=>{
+        //api.massSendNews('ys1SXTdWnZhTZQB77MODarSaJ36xfoTG15deo5aGM3eRvEiuT034AMaUocc66uq9','0',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 获取永久素材列表
+     */
+    getmaterialsAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getMaterials('news',0,10,(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 上传永久素材（缩略图）
+     */
+    uploadmaterialAction(){
+
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        //{"media_id":"Snt_yv6I9f5KMAJKz4GNZAjZSXJeIgZVbUgRey2STH8","url":"https://mmbiz.qlogo.cn/mmbiz/yNHpDQhqmZmaEjdMt6hokMa5ic2a8tjEmDp2tHVAxe3orww1bN4YIiayUBThKC9k3PKyr7OxeZ0vIklb2tMaKDXw/0?wx_fmt=jpeg"}
+        api.uploadMaterial('D:\\webStorm\\CmsWing\\www\\static\\admin\\img\\m0.jpg','thumb',(err,result)=>{
+        //api.uploadThumbMaterial('D:\\webStorm\\CmsWing\\www\\static\\admin\\img\\m21.jpg',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 获取永久素材
+     */
+    getmaterialAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getMaterial('PW4hZzpj-j3qdr_hpRFsePOXn8w4YkAPcKfrOfnvVDCHcYZsp81YZcjLiUMiKg3s',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getmaterial");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 获取临时素材
+     */
+    getmediaAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getMedia('PW4hZzpj-j3qdr_hpRFsePOXn8w4YkAPcKfrOfnvVDCHcYZsp81YZcjLiUMiKg3s',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getmedia");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+        });
+    }
+
+    /**
+     * 上传多媒体文件（图文）
+     */
+    uploadnewsAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.uploadNews({"articles":[{
+                "thumb_media_id":"lEnCBjCXpwLp_Tdh0ImV9KlGZ9Wd3MlTcPVxST3jApIRh2YE1Y9RSNhxEdLKYI7G",
+                "author":"HH",
+                "title":"Happy Day",
+                "content_source_url":"www.qq.com",
+                "content":"hello word",
+                "digest":"test",
+                "show_cover_pic":"1"
+            }]}
+            ,(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"test");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 上传图文消息内的图片获取URL
+     */
+    uploadimageAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.uploadImage('D:\\webStorm\\CmsWing\\www\\static\\admin\\img\\a3.png',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getmaterial");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+    /**
+     * 临时(获取thumb_media_id)
+     */
+    uploadmediaAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.uploadMedia('D:\\webStorm\\CmsWing\\www\\static\\admin\\img\\m0.jpg','thumb',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getmaterial");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+
+/**
+ * 测试
+ */
+    async testAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        let picpath = await this.model('picture').find(34);
+        api.uploadMedia(think.ROOT_PATH+'/www/'+picpath.path,'thumb',(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getmaterial");
+                self.end(result);
+            }else{
+                console.error('err'+err);
+            }
+
+        });
+    }
+    
+    /**
+     * 获取关注者列表
+     */
+    async getusersAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getFollowers((err,result)=>{
+            if(!think.isEmpty(result)){
+                
+                think.log(result,"getuser");
+                self.end(result['data']);
+            }else{
+                Console.error('err'+err)
+            } 
+        });
+    }
+    /**
+     * 获取用户基本信息
+     */
+    async getuserinfoAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.getUser({openid: 'oVe9Ew0zHFp0up1CeNcK2J5RL4xs', lang: 'zh_CN'},(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getuser");
+                self.end(result);
+            }else{
+                Console.error('err'+err)
+            } 
+        });
+    }
+    
+    /**
+     * 批量获取用户基本信息
+     */
+    async getusersinfoAction(){
+        let api = new API('wxec8fffd0880eefbe', 'a084f19ebb6cc5dddd2988106e739a07');
+        let self = this;
+        api.batchGetUsers(['oVe9Ew0zHFp0up1CeNcK2J5RL4xs','oVe9Ewyd7Lw1bKPTtBvCSbB13DtU'],(err,result)=>{
+            if(!think.isEmpty(result)){
+                think.log(result,"getuser");
+                self.end(result);
+            }else{
+                Console.error('err'+err)
+            } 
+        });
+        
+        
+        
+    }
+  
 }
