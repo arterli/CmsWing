b538a691a4f0075ed37e7dbe259d0fadc4ec922d
lf <ljt310@163.com>
2016-02-29 18:45:45 +0800

    aaa

--- src/common/bootstrap/wechatapi.js
+++ src/common/bootstrap/wechatapi.js
@@ -1,9 +1,73 @@
  
  //fdsfds
- /* global bymassend */
-global.bymasssend=(api,media_id,receivers)=>{
+ /* global massend 群发图文消息 */
+global.massSendNews=(api,media_id,receivers)=>{
     let deferred = think.defer();
     api.massSendNews(media_id,receivers,(err,result)=>{
+        //console.log(result)
+        if(!think.isEmpty(result)){
+            deferred.resolve(result);
+            //self.end(result);
+        }else{
+            console.error('err'+err);
+            //deferred.reject(err);
+        }
+    });
+    return deferred.promise;
+}
+
+ /* global massend 群发文本消息 */
+global.massSendText=(api,content,receivers)=>{
+    let deferred = think.defer();
+    api.massSendText(content,receivers,(err,result)=>{
+        //console.log(result)
+        if(!think.isEmpty(result)){
+            deferred.resolve(result);
+            //self.end(result);
+        }else{
+            console.error('err'+err);
+            //deferred.reject(err);
+        }
+    });
+    return deferred.promise;
+}
+
+ /* global massend 群发图片消息 */
+global.massSendImage=(api,media_id,receivers)=>{
+    let deferred = think.defer();
+    api.massSendImage(media_id,receivers,(err,result)=>{
+        //console.log(result)
+        if(!think.isEmpty(result)){
+            deferred.resolve(result);
+            //self.end(result);
+        }else{
+            console.error('err'+err);
+            //deferred.reject(err);
+        }
+    });
+    return deferred.promise;
+}
+
+ /* global massend 群发语音消息 */
+global.massSendVoice=(api,media_id,receivers)=>{
+    let deferred = think.defer();
+    api.massSendVoice(media_id,receivers,(err,result)=>{
+        //console.log(result)
+        if(!think.isEmpty(result)){
+            deferred.resolve(result);
+            //self.end(result);
+        }else{
+            console.error('err'+err);
+            //deferred.reject(err);
+        }
+    });
+    return deferred.promise;
+}
+
+ /* global massend 群发视频消息 */
+global.massSendVideo=(api,media_id,receivers)=>{
+    let deferred = think.defer();
+    api.massSendVideo(media_id,receivers,(err,result)=>{
         //console.log(result)
         if(!think.isEmpty(result)){
             deferred.resolve(result);
