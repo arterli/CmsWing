83aa03882ef6e9c0fb1eb6ed15682445c8b9834c
arterli <arterli@qq.com>
2016-03-07 14:54:35 +0800

    pay

--- src/home/controller/cart.js
+++ src/home/controller/cart.js
@@ -106,13 +106,25 @@ export default class extends Base {
           return this.fail("你木有登录！")
       }
       if(think.isEmpty(this.cart.data)){ return this.fail("木有宝贝提交啥订单呢？") }
-      this.meta_title = "确认订单信息";//标题1
-    this.keywords = this.setup.WEB_SITE_KEYWORD ? this.setup.WEB_SITE_KEYWORD : '';//seo关键词
-    this.description = this.setup.WEB_SITE_DESCRIPTION ? this.setup.WEB_SITE_DESCRIPTION : "";//seo描述
-    
+      
+      let order_amount;//订单金额
+      let payable_amount;//应付金额，商品的原价
+      let real_amount;//商品参与获得的价格
+      let payable_freight;//应付运费
+      let real_freight//实际运费
+      
+      //TODO购物清单 todo
+      //购物车Post过来的商品id;暂时去购物车内所有的商品
+      //购物车内的宝贝
       //let cart_goods = await this.model("cart").where({uid:this.user.uid}).select();
       let cart_goods = this.cartdata;
-      
+      //应付金额
+      let parr = [];
+      for(let val of cart_goods){
+          parr.push(val.price);
+      }
+      console.log(parr);
+      real_amount = eval(parr.join('+'));
       //联系人
       let addrlist = await this.model("address").where({user_id:this.user.uid}).order("is_default DESC").select();
       for(let val of addrlist){
@@ -127,14 +139,30 @@ export default class extends Base {
            val.logo =  await this.model("pay_plugin").where({id:val.plugin_id}).getField("logo",true);
         }
       this.assign("paylist",paylist);
-        //购物清单 todo
-        //购物车Post过来的商品id;暂时去购物车内所有的商品
         
         
+       //运费计算
+        //    1、如果店铺只使用统一运费，那么顾客下单计算时按最低运费收取。
+        //    2、如果店铺只使用一种运费模板规则，那么顾客下单计算时均按此规则收取运费。
+        //    3、如果店铺使用了不同的运费模板规则，那么顾客下单时各运费模板规则先单独计算运费再叠加。
+        //    4、如果店铺同时使用统一运费和不同的运费模板规则，那么顾客下单时统一运费单独计算运费，不同的运费模板
+       //TODO
+       real_freight = 8;
+       this.assign("real_freight",real_freight);
+       //订单促销优惠信息
+       //TODO
+       
         
-        
+        //订单金融 实付金额+邮费-订单优惠金额
+        //TODO
+        console.log(real_amount);
+        order_amount =Number(real_amount) + Number(real_freight)
+        this.assign("order_amount",order_amount);
        //this.end(cart_goods);
-      return this.display();
+        this.meta_title = "确认订单信息";//标题1
+        this.keywords = this.setup.WEB_SITE_KEYWORD ? this.setup.WEB_SITE_KEYWORD : '';//seo关键词
+        this.description = this.setup.WEB_SITE_DESCRIPTION ? this.setup.WEB_SITE_DESCRIPTION : "";//seo描述
+        return this.display();
       
   }
   //获取省市三级联动
@@ -241,6 +269,11 @@ async editaddrmodalAction(){
 async createorderAction(){
     if(!this.is_login){return this.fail("你木有登录！")};
     let data = this.post();
+    let order_amount;//订单金额
+    let payable_amount;//应付金额，商品的原价
+    let real_amount;//商品参与获得的价格
+    let payable_freight;//应付运费
+    let real_freight//实际运费
     //判断购物车内是否有商品如果没有停止执行，如果有则删除
     let goodsids;
     let goodslist =JSON.parse(data.goodslist);
@@ -266,10 +299,43 @@ async createorderAction(){
     //添加送货地址
     let address = await this.model("address").fieldReverse("id,user_id,is_default").find(data.address);
     data = think.extend(data,address);
-    
-    
-    //生成订单
-    let order_id = await this.model("order").add(data);
+
+     //应付金额
+      let parr = [];
+      for(let val of isgoods){
+          parr.push(val.price);
+      }
+      console.log(parr);
+      real_amount = eval(parr.join('+'));
+      data.real_amount = real_amount;
+      
+     //运费计算
+        //    1、如果店铺只使用统一运费，那么顾客下单计算时按最低运费收取。
+        //    2、如果店铺只使用一种运费模板规则，那么顾客下单计算时均按此规则收取运费。
+        //    3、如果店铺使用了不同的运费模板规则，那么顾客下单时各运费模板规则先单独计算运费再叠加。
+        //    4、如果店铺同时使用统一运费和不同的运费模板规则，那么顾客下单时统一运费单独计算运费，不同的运费模板
+       //TODO
+       real_freight = 8;
+       data.real_freight = real_freight;
+       
+       
+        
+        //支付状态 pay_stayus 0:未付款 ,1:已付款
+        data.pay_status = 0;
+        //订单状态 status 2:等待审核，3:已审核
+        data.status = 2;
+        //发货状态 delivery_status 0:未发货，1:已发货
+        data.delivery_status = 0;
+        //订单创建时间 create_time
+        data.create_time = new Date().valueOf();
+        
+        //订单金融 实付金额+邮费-订单优惠金额
+        //TODO
+        console.log(real_amount);
+        order_amount =Number(real_amount) + Number(real_freight)
+        data.order_amount = order_amount;
+        //生成订单
+       let order_id = await this.model("order").add(data);
     
     //储存宝贝
     //let order_id = 22;
@@ -288,6 +354,31 @@ async createorderAction(){
     console.log(ngoods);
     await this.model("order_goods").addMany(ngoods);
     console.log(data);
-    this.end(order_id);
+    
+    return this.success({name:'订单创建成功，正在跳转支付页面！',url:`/home/cart/pay/order/${order_id}/setp/3`});
+    
 }
+   //支付
+ async  payAction(){
+       if(!this.is_login){return this.fail("你木有登录！")};
+       let order_id = this.get("order");
+       let setp = this.get("setp")||"";
+       //this.end(order_id  + "=" + setp)
+       //订单信息
+       let order = await this.model("order").where({user_id:this.user.uid}).find(order_id);
+       console.log(order);
+       this.assign("order",order);
+       
+        //支付方式
+      let paylist = await this.model("payment").where({status:1}).order("sort ASC").select();
+      for(let val of paylist){
+           val.logo =  await this.model("pay_plugin").where({id:val.plugin_id}).getField("logo",true);
+        }
+        this.assign("paylist",paylist);
+        this.assign("setp",setp);
+        this.meta_title = "订单支付";//标题1
+        this.keywords = this.setup.WEB_SITE_KEYWORD ? this.setup.WEB_SITE_KEYWORD : '';//seo关键词
+        this.description = this.setup.WEB_SITE_DESCRIPTION ? this.setup.WEB_SITE_DESCRIPTION : "";//seo描述
+        return this.display();
+   }
 }
--- www/static/assets/js/scripts.js
+++ www/static/assets/js/scripts.js
@@ -117,6 +117,7 @@
         _cart();
         _login();
         _swal();
+        _ajax_post();
 		/** Bootstrap Tooltip **/ 
 		jQuery("a[data-toggle=tooltip], button[data-toggle=tooltip], span[data-toggle=tooltip]").tooltip();
 	}
@@ -136,7 +137,115 @@
       }
     ); 
   }
-    
+ 
+    /**
+ * ajax post submit请求
+ * <form class = "form-horizontal">
+ * <button target-form="form-horizontal" type="submit" class="ajax-post">确定</button>
+ * confirm,
+ *****************************************************************************************************************************/
+function _ajax_post() {
+    $('.ajax-post').click(function(){
+
+    var target,query,form;
+    var target_form = $(this).attr('target-form');
+    var that = this;
+    var nead_confirm=false;
+    if(($(this).attr('type')=='submit') || (target = $(this).attr('href')) || (target = $(this).attr('url')) ){
+        form = $('.'+target_form);
+        if ($(this).attr('hide-data') === 'true'){//无数据时也可以使用的功能
+            form = $('.hide-data');
+            query = form.serialize();
+        }else if (form.get(0)==undefined){
+            return false;
+        }else if ( form.get(0).nodeName=='FORM' ){
+            //表单验证
+            // $('[data-validate="parsley"]').parsley().validate();
+            // if(true !== $('[data-validate="parsley"]').parsley().isValid()){
+            //     return false;
+            // }
+            if ( $(this).hasClass('confirm') ) {
+                if(!confirm('确认要执行该操作吗?')){
+                    return false;
+                }
+            }
+            if($(this).attr('url') !== undefined){
+                target = $(this).attr('url');
+            }else{
+                target = form.get(0).action;
+            }
+            query = form.serialize();
+        }else if( form.get(0).nodeName=='INPUT' || form.get(0).nodeName=='SELECT' || form.get(0).nodeName=='TEXTAREA') {
+
+            form.each(function(k,v){
+                if(v.type=='checkbox' && v.checked==true){
+
+                    nead_confirm = true;
+                }
+            })
+            if ( nead_confirm && $(this).hasClass('confirm') ) {
+                if(!confirm('确认要执行该操作吗?')){
+                    return false;
+                }
+            }
+            query = form.serialize();
+        }else{
+            if ( $(this).hasClass('confirm') ) {
+                if(!confirm('确认要执行该操作吗?')){
+                    return false;
+                }
+            }
+            query = form.find('input,select,textarea').serialize();
+        }
+        $(that).addClass('disabled').attr('autocomplete','off').prop('disabled',true);
+        $.post(target,query).success(function(data){
+            //console.log(data)
+            //return false;
+            if (data.errno==0) {
+                if (data.data.url) {
+                     _toastr(data.data.name + ' 页面即将自动跳转~',"top-right","success",false);
+                   // toastr.success(data.data.name + ' 页面即将自动跳转~');
+                }else{
+                    _toastr(data.data.name,"top-right","success",false);
+                    //toastr.success(data.data.name);
+                }
+                setTimeout(function(){
+                    $(that).removeClass('disabled').prop('disabled',false);
+                    if (data.data.url) {
+                        location.href=data.data.url;
+                    }else if( $(that).hasClass('no-refresh')){
+                        //toastr.clear()
+                    }else{
+                        location.reload();
+                    }
+                },1500);
+            }else{
+                if(data.errno==1001){
+                    $.each(data.errmsg,function(i,n){
+                        _toastr(n,"top-right","error",false);
+                        //toastr.error(n);
+                    })
+                }else {
+                    _toastr(data.errmsg,"top-right","error",false);
+                    //toastr.error(data.errmsg);
+                }
+                console.log(data);
+
+                setTimeout(function(){
+                    $(that).removeClass('disabled').prop('disabled',false);
+                    if (data.data) {
+                        location.href=data.data;
+                    }else{
+                        //toastr.clear()
+                    }
+                },1500);
+            }
+        });
+    }
+    return false;
+});
+}
+
 
 /** Preloader
  **************************************************************** **/
