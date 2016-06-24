d4fd74736d1e77ade076aa86d5356e51548f1080
Arterli <arterli@qq.com>
2016-06-24 11:17:42 +0800

    分类信息

--- src/admin/controller/type.js
+++ src/admin/controller/type.js
@@ -24,13 +24,21 @@ export default class extends Base {
  async indexAction(){
     //auto render template file index_index.html
     let type = this.model("type");
-    let list = await type.select();
+    let list = await type.order('displayorder ASC,typeid DESC').select();
     //console.log(list);
     this.assign("list",list);
     this.meta_title="分类管理"
     return this.display();
   }
-
+    //分类信息设置
+ async typeviewAction(){
+     let id = this.get("typeid");
+     let info = await this.model('type').find({typeid:id});
+     this.active="admin/type/index"
+     console.log(info);
+     this.meta_title=`${info.name}-分类设置`
+  return this.display();
+ }
   /**
    * topic action
    *
@@ -49,8 +57,24 @@ export default class extends Base {
   /**
    * 更新/修改数据
    */
-  updateAction(){
-    console.log(this.post())
-    //todo
+ async updateAction(){
+   let data = this.post("data");
+      data = JSON.parse(data);
+      console.log(data);
+      for(let val of data){
+          //添加
+          if(val.isdel==0 && val.name != 0 && val.typeid ==0){//添加
+             this.model('type').add(val);
+          }else if(val.isdel==0 && val.name != 0 && val.typeid !=0){//更新
+              this.model('type').update(val,{typeid:val.typeid});
+          }else if(val.isdel == 1){
+              this.model('type').delete({
+                  where: {typeid: val.typeid}
+              })
+          }
+      }
+
+      //todo
+      return this.success({name:"操作成功"})
   }
 }
