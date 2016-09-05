9f91a3b4812c2c74059cd522c1388cabe5d944ac
Arterli <arterli@qq.com>
2016-09-05 14:27:02 +0800

    完成手机端用户中心页面重构

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -17,8 +17,7 @@ export default class extends Base {
    * @return {Promise} []
    */
   async indexAction(){
-   
-    //auto render template file index_index.html
+      //auto render template file index_index.html
     this.meta_title = "首页";//标题1
     this.keywords = this.setup.WEB_SITE_KEYWORD ? this.setup.WEB_SITE_KEYWORD : '';//seo关键词
     this.description = this.setup.WEB_SITE_DESCRIPTION ? this.setup.WEB_SITE_DESCRIPTION : "";//seo描述
