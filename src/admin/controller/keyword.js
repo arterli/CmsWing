83ec56a5e9135992e267a2deeab88d076808a166
Arterli <arterli@qq.com>
2016-10-17 15:32:29 +0800

    新增后台对独立模型的开发管理，话题的后台管理。

---
+++ src/admin/controller/keyword.js
@@ -0,0 +1,26 @@
+// +----------------------------------------------------------------------
+// | CmsWing [ 网站内容管理框架 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015 http://www.cmswing.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: arterli <arterli@qq.com>
+// +----------------------------------------------------------------------
+'use strict';
+
+import Base from './base.js';
+
+export default class extends Base {
+  init(http) {
+    super.init(http);
+    this.tactive = "article";
+
+  }
+  /** 话题控制器
+   * index action
+   * @return {Promise} []
+   */
+  indexAction(){
+    //auto render template file index_index.html
+    return this.display();
+  }
+}
--- src/admin/controller/model.js
+++ src/admin/controller/model.js
@@ -50,11 +50,33 @@ export default class extends Base {
             }
         } else {
             this.active = "admin/model/index"
-            this.meta_title = "添加模型"
+            this.meta_title = "添加系统模型"
             return this.display()
         }
     }
-
+    /**
+     * 新建独立模型
+     * @returns {*}
+     */
+    async addextAction() {
+        if (this.isPost()) {
+            let data = this.post();
+            //console.log(data);
+            data.create_time = new Date().valueOf();
+            data.update_time = new Date().valueOf();
+            data.status = 1
+            let res = await this.db.add(data);
+            if (res) {
+                this.cache("get_document_model", null);//清除模型缓存
+                this.cache("get_model", null);//清除模型缓存
+                return this.success({name: "添加成功", url: "/admin/model/index"});
+            }
+        } else {
+            this.active = "admin/model/index"
+            this.meta_title = "添加独立模型"
+            return this.display()
+        }
+    }
     /**
      * 编辑模型
      *
@@ -142,6 +164,92 @@ export default class extends Base {
         }
     }
 
+    /**
+     * 编辑独立模型
+     *
+     */
+    async editextAction() {
+        if (this.isPost()) {
+            let post = this.post()
+            post.update_time = new Date().valueOf();
+            if(think.isArray(post.attribute_list)){
+                post.attribute_list=post.attribute_list.join(",");
+            }
+
+            let res =await this.db.update(post);
+            if(res){
+                this.cache("get_document_model", null);//清除模型缓存
+                this.cache("get_model", null);//清除模型缓存
+                return this.success({name:"更新模型成功!",url: "/admin/model/index"})
+            }
+        } else {
+            let id = this.get("id");
+            let allfields;
+            if (think.isEmpty(id)) {
+                this.fail('参数不能为空！');
+            }
+            let data = await this.db.find(id);
+            // console.log(data);
+            data.attribute_list = think.isEmpty(data.attribute_list) ? '' : data.attribute_list.split(",");
+            // console.log(data.attribute_list);
+            let fields = await this.model('attribute').where({model_id: data.id}).field('id,name,title,is_show').select();
+            //是否继承了其他模型
+            if (data.extend != 0) {
+                var extend_fields = await this.model('attribute').where({model_id: data.extend}).field('id,name,title,is_show').select();
+                allfields = fields.concat(extend_fields);
+            } else {
+                allfields = fields;
+            }
+            // console.log(allfields)
+            //梳理属性的可见性
+            for (let field of allfields) {
+                if (!think.isEmpty(data.attribute_list) && !in_array(field.id, data.attribute_list)) {
+                    field.is_show = 0;
+                }
+                //console.log(field);
+            }
+            //console.log(allfields);
+            //改造数组
+            var obj = {}
+            if (allfields) {
+                for (let v of allfields) {
+                    obj[v.id] = v;
+                }
+            } else {
+                for (let v of fields) {
+                    obj[v.id] = v;
+                }
+            }
+            //获取模型排序字段
+            let field_sort = JSON.parse(data.field_sort);
+            if (!think.isEmpty(field_sort)) {
+                for (let group in field_sort) {
+                    //console.log(field_sort[group])
+                    //for(var value of field_sort[group]){
+                    //    console.log(value)
+                    //}
+
+                    field_sort[group].forEach((v, k)=> {
+                        if(obj[v]){
+                            obj[v].group = group;
+                            obj[v].sort = k;
+                        }
+                        //console.log(v, k)
+                    })
+
+                }
+            }
+
+            let order = think._.values(obj);
+            //console.log(order);
+            let orderbgy = think._.orderBy(order, ['group', 'sort'], ['asc', 'asc']);
+
+            this.assign({'fields': fields, 'extend_fields': extend_fields, 'allfields': orderbgy, 'info': data})
+            this.active = "admin/model/index"
+            this.meta_title = "编辑模型"
+            this.display();
+        }
+    }
     /**
      * 生成模型
      * @returns {*}
