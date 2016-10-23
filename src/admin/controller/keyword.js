fd1c23ac266b901d8a2d1e8c6166c482afdc5168
Arterli <arterli@qq.com>
2016-10-23 23:46:47 +0800

    后台话题管理列表

--- src/admin/controller/keyword.js
+++ src/admin/controller/keyword.js
@@ -19,8 +19,34 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  async indexAction(){
+    //搜索
+    let map = {};
+    if(this.get("title")){
+      map.title=["like","%"+this.get("title")+"%"]
+    }
+    let list = await this.model("keyword").where(map).order('discuss_count_update DESC').page(this.get("page"),20).countSelect();
+    for (let v of list.data){
+      v.lastuser = await this.model("keyword_data").where({tagid:v.id}).order("add_time DESC").getField("uid",true);
+    }
+    let Pages = think.adapter("pages", "page"); //加载名为 dot 的 Template Adapter
+    let pages = new Pages(); //实例化 Adapter
+    let page = pages.pages(list);
+    this.assign('list', list);
+    this.assign('pagerData', page); //分页展示使用
+    this.meta_title="话题管理";
     //auto render template file index_index.html
     return this.display();
+  }
+  //锁定话题
+  async lockAction(){
+    let id = this.get("ids");
+    let lock = this.get("lock");
+    let up = await this.model("keyword").where({id:id}).update({lock:lock});
+    if(up){
+      return this.success({name:"操作成功!"});
+    }else {
+      return this.fail("操作失败!")
+    }
   }
 }
