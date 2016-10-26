506b8badf52fc050bbea8c6a0c2cd5431cb6ba9a
Arterli <arterli@qq.com>
2016-10-26 15:39:38 +0800

    后台分页bug

--- src/admin/controller/finance.js
+++ src/admin/controller/finance.js
@@ -30,7 +30,7 @@ export default class extends Base {
     let list = await this.model("balance_log").order("id DESC").page(this.get('page')).countSelect();
     //console.log(list);
     let _pages = think.adapter("pages","page");
-    let pages = new _pages();
+    let pages = new _pages(this.http);
     let page = pages.pages(list);
     this.assign("pagerData",page);
     for(let itme of list.data){
