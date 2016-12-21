c5625c18492db259cb02c010fb0b7fdb3209ee54
Arterli <arterli@qq.com>
2016-12-21 18:44:39 +0800

    全站搜索后台管理

--- src/admin/controller/search.js
+++ src/admin/controller/search.js
@@ -13,12 +13,63 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  async indexAction(){
     //auto render template file index_index.html
+      let list = await this.model("search_model").select();
+      this.assign("list",list);
       this.meta_title = "全站搜索";
     return this.display();
   }
 
+    /**
+     * 添加搜索分类
+     * @returns {Promise.<void>}
+     */
+  async addAction(){
+      if(this.isPost()){
+          let data = this.post();
+          let extend = await this.model("model").get_model(data.mod,"extend");
+          data.extend = extend;
+          let add = await this.model("search_model").add(data);
+          if(add){
+              return this.success({name:"添加成功!"})
+          }else {
+              return this.fail("添加失败!");
+          }
+
+      }else {
+          let modlist = await this.model("model").where({status:1,id:[">",1]}).select();
+          //console.log(modlist);
+          this.assign("modlist",modlist);
+          this.meta_title="添加搜索分类";
+          return this.display();
+      }
+
+  }
+  async editAction(){
+      if(this.isPost()){
+          let data = this.post();
+          let extend = await this.model("model").get_model(data.mod,"extend");
+          data.extend = extend;
+          let up = await this.model("search_model").update(data);
+          if(up){
+              return this.success({name:"编辑成功!"})
+          }else {
+              return this.fail("编辑失败!");
+          }
+      }else {
+          let info = await this.model("search_model").find(this.get("id"));
+          this.assign("info",info);
+          let modlist = await this.model("model").where({status:1,id:[">",1]}).select();
+          this.assign("modlist",modlist);
+          this.meta_title = "编辑搜索分类";
+          return this.display();
+      }
+  }
+    /**
+     * 重建索引
+     * @returns {Promise.<void>}
+     */
    async createindexAction(){
        let paths = think.RESOURCE_PATH + "/backup/";
        let lock = paths + "createindex.lock";
@@ -32,10 +83,15 @@ export default class extends Base {
               //创建锁文件
               fs.writeFileSync(lock, new Date())
           }
-          //查询重建索引的表
-           let tables = [{table:"document",data:"title,keyname",mod:2,extend:1,name:"文章",pk:"id",addtime:"create_time"},
-               {table:"document",data:"title,keyname",mod:3,extend:1,name:"下载",pk:"id",addtime:"create_time"},
-               {table:"document",data:"title,keyname",mod:4,extend:1,name:"商品",pk:"id",addtime:"create_time"}];
+           let tables = await this.model("search_model").select();
+           for(let v of tables){
+               if(v.extend==0){
+                   v.table= await this.model("model").get_model(v.mod,"name");
+               }else {
+                   v.table="document";
+               }
+           }
+
            await this.session('createindex_tables', tables);
            //清空缓存表
            await this.model("search").where("1=1").delete();
