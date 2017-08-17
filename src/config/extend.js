948e5ca788a84f1ab46b4f495d40f0f4f6e1a638
arter <arterli@qq.com>
2017-08-17 17:19:37 +0800

    升级到thinkjs3.0正式版

--- src/config/extend.js
+++ src/config/extend.js
@@ -2,10 +2,14 @@ const view = require('think-view');
 const model = require('think-model');
 const fetch = require('think-fetch');
 const email = require('think-email');
+const cache = require('think-cache');
+const session = require('think-session');
 module.exports = [
   view, //make application support view
   model(think.app),
   fetch, // HTTP request client.
-  email
+  email,
+  cache,
+  session
 ];
 
