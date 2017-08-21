5aa1702cf7922ebaf2b67ef3a8de3f382f0d7c64
arter <arterli@qq.com>
2017-08-21 11:08:35 +0800

    增加log 配置

--- src/config/adapter.js
+++ src/config/adapter.js
@@ -1,6 +1,5 @@
 const fileCache = require('think-cache-file');
 const fileSession = require('think-session-file');
-const {File} = require('think-logger3');
 const path = require('path');
 //const isDev = think.env === 'development';
 /**
@@ -50,17 +49,3 @@ exports.session = {
  * @type {Object}
  */
 exports.view = require("./adapter/view");
-/**
- * 
- * @type {{type: string, file: {handle: Logger.File, backups: number, absolute: boolean, maxLogSize: number, filename}}}
- */
-exports.logger = {
-    type: 'file',
-    file: {
-        handle: File,
-        backups: 10,
-        absolute: true,
-        maxLogSize: 50 * 1024,  //50M
-        filename: path.join(think.ROOT_PATH, 'logs/xx.log')
-    }
-}
