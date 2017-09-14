d9b227054a263a41d8a9ff3d1fe64280fd1f6774
arter <arterli@qq.com>
2017-09-14 19:25:56 +0800

    修复后台栏目管理报错的问题

--- src/controller/admin/category.js
+++ src/controller/admin/category.js
@@ -16,7 +16,7 @@ module.exports = class extends Base {
   constructor(ctx) {
     super(ctx); // 调用父级的 constructor 方法，并把 ctx 传递进去
     // 其他额外的操作
-    this.db = this.model('category');
+    this.db = this.model('cmswing/category');
     this.tactive = 'article';
   }
   async indexAction() {
