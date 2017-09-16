595b4be666f6e38982a67bc3e3a40dba7194c7db
arter <arterli@qq.com>
2017-09-16 13:54:23 +0800

    修复api

--- src/controller/api/category.js
+++ src/controller/api/category.js
@@ -1,18 +1,19 @@
 module.exports = class extends think.cmswing.rest {
   /**
-     * 获取分类信息
-     * /api/category 获取全部栏目（树结构）
-     * /api/category/1 获取栏目id为1的栏目信息
-     * @returns {Promise.<*>}
-     */
+   * 获取分类信息
+   * /api/category 获取全部栏目（树结构）
+   * /api/category/1 获取栏目id为1的栏目信息
+   * @returns {Promise.<*>}
+   */
   async getAction() {
     let data;
+    const model = this.model('cmswing/category');
     if (this.id) {
-      const pk = await this.modelInstance.pk;
-      data = await this.modelInstance.where({[pk]: this.id}).find();
+
+      data = await model.where({id: this.id}).find();
       return this.success(data);
     }
-    data = await this.modelInstance.get_all_category();
+    data = await model.get_all_category();
     return this.success(arr_to_tree(data, 0));
   }
   async postAction() {
