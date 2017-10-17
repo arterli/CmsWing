35535a7150143a1acaa4963e7b24c71bdf34bfc2
孙会鹏 <1571360448@qq.com>
2017-10-17 10:33:26 +0800

    本地数据库更改

--- src/config/adapter/model.js
+++ src/config/adapter/model.js
@@ -16,10 +16,10 @@ module.exports = {
   mysql: {
     handle: mysql, // Adapter handle
     user: 'root', // 用户名
-    password: '', // 密码
+    password: 'root', // 密码
     database: 'cmswing', // 数据库
     host: '127.0.0.1', // host
-    port: 3306, // 端口
+    port: 3307, // 端口
     connectionLimit: 1, // 连接池的连接个数，默认为 1
     prefix: 'cmswing_', // 数据表前缀，如果一个数据库里有多个项目，那项目之间的数据表可以通过前缀来区分
     cache: { // 额外的缓存配置
