0d55d3ef5bb72760250c5cb7d6b90848ec3b1e53
Arterli <arterli@qq.com>
2017-10-21 03:36:27 +0800

    编辑器，移动到插件，新增wangEditor ,Markdown 编辑器，后台可切换。新增文档模型

---
+++ src/controller/ext/editor/admin.js
@@ -0,0 +1,41 @@
+// +----------------------------------------------------------------------
+// | CmsWing [ 网站内容管理框架 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015-2115 http://www.cmswing.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: arterli <arterli@qq.com>
+// +----------------------------------------------------------------------
+// 本文件不能删除
+module.exports = class extends think.cmswing.extAdmin {
+  /**
+   * index action
+   * 插件管理入口
+   * @return {Promise} []
+   */
+  async indexAction() {
+    // 分页列表实例
+
+    // let data = await this.model("ext_表名").page(this.get('page')).countSelect();
+    // let Pages = think.adapter("pages", "page"); //加载名为 dot 的 Template Adapter
+    // let pages = new Pages(this.http); //实例化 Adapter
+    // let page = pages.pages(data);
+    // this.assign('pagerData', page); //分页展示使用
+    // this.assign('list', data.data);
+
+    // 获取当前插件的分类,插件如有分类的需求，直接调用。
+
+    // await this.gettype()
+    // const list = await this.extModel('demo').select();
+    // console.log(list);
+    // 入口模版渲染
+    return this.extDisplay();
+  }
+  // 禁止卸载
+  async uninstallAction() {
+    return this.fail('禁止卸载!');
+  }
+  // 禁止删除
+  async delextAction() {
+    return this.fail('禁止删除!');
+  }
+};
---
+++ src/controller/ext/editor/hooks.js
@@ -0,0 +1,155 @@
+// hooks
+module.exports = class extends think.cmswing.extIndex {
+  /**
+   * 后台编辑器钩子方法
+   * 【视图】
+   * @param ...val
+   */
+  async adminEdit(name, value, type = false) {
+    // 钩子业务处理
+    const setting = await this.extConfig('editor');
+    if (type && !think.isEmpty(type.$hook_type)) {
+      const t = type.$hook_type.split('_');
+      if (!think.isEmpty(t[0])) {
+        setting.type = t[0];
+      }
+      if (!think.isEmpty(t[1])) {
+        setting.wysiwyg = t[1];
+      }
+      if (!think.isEmpty(t[2])) {
+        setting.height = `${t[2]}px`;
+      }
+    }
+    this.assign({
+      name: name,
+      value: value,
+      setting: setting
+    });
+    const html = await this.hookRender('adminEdit', 'editor');
+    return html;
+  }
+  /**
+     * 后台编辑器钩子方法
+     * 【视图】
+     * @param ...val
+     */
+  async homeEdit(name, value, type = false) {
+      // 钩子业务处理
+    const setting = await this.extConfig('editor');
+    if (type && !think.isEmpty(type.$hook_type)) {
+      const t = type.$hook_type.split('_');
+      if (!think.isEmpty(t[0])) {
+        setting.type = t[0];
+      }
+      if (!think.isEmpty(t[1])) {
+        setting.wysiwyg = t[1];
+      }
+      if (!think.isEmpty(t[2])) {
+        setting.height = `${t[2]}px`;
+      }
+    }
+    this.assign({
+      name: name,
+      value: value,
+      setting: setting
+    });
+    const html = await this.hookRender('homeEdit', 'editor');
+    return html;
+  }
+
+  /**
+     * 页面header钩子，一般用于加载插件CSS文件和代码
+     * @param name
+     * @param value
+     * @param type
+     * @returns {Promise.<void>}
+     */
+  async pageHeader(name, value, type = false) {
+    // 钩子业务处理
+    const setting = await this.extConfig('editor');
+    if (type && !think.isEmpty(type.$hook_type)) {
+      const t = type.$hook_type.split('_');
+      if (!think.isEmpty(t[0])) {
+        setting.type = t[0];
+      }
+      if (!think.isEmpty(t[1])) {
+        setting.wysiwyg = t[1];
+      }
+      if (!think.isEmpty(t[2])) {
+        setting.height = `${t[2]}px`;
+      }
+    }
+    this.assign({
+      name: name,
+      value: value,
+      setting: setting
+    });
+    const html = await this.hookRender('pageHeader', 'editor');
+    return html;
+  }
+
+  /**
+     * 页面footer钩子，一般用于加载插件JS文件和JS代码
+     * @param name
+     * @param value
+     * @param type
+     * @returns {Promise.<*>}
+     */
+  async pageFooter(name, value, type = false) {
+    // 钩子业务处理
+    const setting = await this.extConfig('editor');
+    if (type && !think.isEmpty(type.$hook_type)) {
+      const t = type.$hook_type.split('_');
+      if (!think.isEmpty(t[0])) {
+        setting.type = t[0];
+      }
+      if (!think.isEmpty(t[1])) {
+        setting.wysiwyg = t[1];
+      }
+      if (!think.isEmpty(t[2])) {
+        setting.height = `${t[2]}px`;
+      }
+    }
+    this.assign({
+      name: name,
+      value: value,
+      setting: setting
+    });
+    const html = await this.hookRender('pageFooter', 'editor');
+    return html;
+  }
+
+  /**
+     *
+     * @param name
+     * @param value
+     * @param type
+     * @returns {Promise.<*>}
+     */
+  async pageContent(name, value, type = false) {
+    // 钩子业务处理
+    const setting = await this.extConfig('editor');
+    if (type && !think.isEmpty(type.$hook_type)) {
+      const t = type.$hook_type.split('_');
+      if (!think.isEmpty(t[0])) {
+        setting.type = t[0];
+      }
+      if (!think.isEmpty(t[1])) {
+        setting.wysiwyg = t[1];
+      }
+      if (!think.isEmpty(t[2])) {
+        setting.height = `${t[2]}px`;
+      }
+    }
+    if (setting.type == 2 && setting.wysiwyg == 1) {
+      value = value.split('_ueditor_page_break_tag_');
+    }
+    this.assign({
+      name: name,
+      value: value,
+      setting: setting
+    });
+    const html = await this.hookRender('pageContent', 'editor');
+    return html;
+  }
+};
