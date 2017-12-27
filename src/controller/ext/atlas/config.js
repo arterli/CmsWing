66264113918924c2502c5f49c61e0db0c30f82bc
arter <arterli@qq.com>
2017-12-27 18:45:06 +0800

    修复多图上传和图集插件，删除图片报错的问题

--- src/controller/ext/atlas/config.js
+++ src/controller/ext/atlas/config.js
@@ -4,7 +4,7 @@ module.exports = {
   description: '用于图片模型多图上传', // 插件描述
   isadm: 0, // 是否有后台管理，1：有，0：没有，入口地址:'/ext/atlas/admin/index'
   isindex: 0, // 是否需要前台访问，1：需要，0：不需要,入口地址:'/ext/atlas/index/index'
-  version: '1.0', // 版本号
+  version: '1.1', // 版本号
   author: 'cmswing', // 作者
   table: [], // 插件包含的 数据库表，不包含表前缀，如：cmswing_ext_table 就是 table，多个['table','table_2']没有留空数组。
   sql: '', // 插件安装的时候会找个名字的sql文件导入，默认 插件目录名.sql;
